7db82be2af22fda74c386c2cca39c4e9e1784259
unknown <hugo.ahmed@enti.cat>
2024-05-19 20:21:34 +0200

    Asteroids archivo creado
    falla el getX y getY del player

---
+++ SDL/asteroid.h
@@ -0,0 +1,18 @@
+#define ASTEROID_H
+#include <cmath>
+#include "Player.h"
+
+class Asteroid {
+private:
+    float x, y;
+    float speed;
+    float directionX, directionY;
+
+public:
+    Asteroid(float x, float y, float speed);
+
+    void updatePosition(const Player& player);
+    void render(SDL_Renderer* renderer);
+    float getX() const { return x; }
+    float getY() const { return y; }
+};
